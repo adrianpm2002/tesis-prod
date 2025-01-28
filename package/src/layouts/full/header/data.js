154d1474f5a7dc8fe89d1d122495f6096c7abfef
adrianpm2002 <adrianpascual0708@gmail.com>
2025-01-27 19:26:39 -0500

    fix:build

--- package/src/layouts/full/header/data.js
+++ package/src/layouts/full/header/data.js
@@ -1,16 +1,4 @@
 
-import icon1 from 'src/assets/images/svgs/icon-account.svg';
-import icon2 from 'src/assets/images/svgs/icon-inbox.svg';
-import icon3 from 'src/assets/images/svgs/icon-tasks.svg';
-import ddIcon1 from 'src/assets/images/svgs/icon-dd-chat.svg';
-import ddIcon2 from 'src/assets/images/svgs/icon-dd-cart.svg';
-import ddIcon3 from 'src/assets/images/svgs/icon-dd-invoice.svg';
-import ddIcon4 from 'src/assets/images/svgs/icon-dd-date.svg';
-import ddIcon5 from 'src/assets/images/svgs/icon-dd-mobile.svg';
-import ddIcon6 from 'src/assets/images/svgs/icon-dd-lifebuoy.svg';
-import ddIcon7 from 'src/assets/images/svgs/icon-dd-message-box.svg';
-import ddIcon8 from 'src/assets/images/svgs/icon-dd-application.svg';
-
 // Nueva configuración de alertas
 const notifications = [
   {
@@ -45,79 +33,6 @@ const notifications = [
   },
 ];
 
-// Profile dropdown
-const profile = [
-  {
-    href: '/user-profile',
-    title: 'Mi Perfil',
-    subtitle: 'Account Settings',
-    icon: icon1,
-  },
-  {
-    href: '/apps/email',
-    title: 'Mis mensajes',
-    subtitle: 'Messages & Emails',
-    icon: icon2,
-  },
-  {
-    href: '/apps/notes',
-    title: 'Mis Tareas',
-    subtitle: 'To-do and Daily Tasks',
-    icon: icon3,
-  },
-];
-
-// apps dropdown
-const appsLink = [
-  {
-    href: '/apps/chats',
-    title: 'Chat Application',
-    subtext: 'Messages & Emails',
-    avatar: ddIcon1,
-  },
-  {
-    href: '/apps/ecommerce/shop',
-    title: 'eCommerce App',
-    subtext: 'Messages & Emails',
-    avatar: ddIcon2,
-  },
-  {
-    href: '/',
-    title: 'Invoice App',
-    subtext: 'Messages & Emails',
-    avatar: ddIcon3,
-  },
-  {
-    href: '/apps/calendar',
-    title: 'Calendar App',
-    subtext: 'Messages & Emails',
-    avatar: ddIcon4,
-  },
-  {
-    href: '/apps/contacts',
-    title: 'Contact Application',
-    subtext: 'Account settings',
-    avatar: ddIcon5,
-  },
-  {
-    href: '/apps/tickets',
-    title: 'Tickets App',
-    subtext: 'Account settings',
-    avatar: ddIcon6,
-  },
-  {
-    href: '/apps/email',
-    title: 'Email App',
-    subtext: 'To-do and Daily tasks',
-    avatar: ddIcon7,
-  },
-  {
-    href: '/',
-    title: 'Kanban Application',
-    subtext: 'To-do and Daily tasks',
-    avatar: ddIcon8,
-  },
-];
 
 const pageLinks = [
   {
@@ -154,4 +69,4 @@ const pageLinks = [
   },
 ];
 
-export { notifications, profile, pageLinks, appsLink };
+export { notifications, pageLinks };
