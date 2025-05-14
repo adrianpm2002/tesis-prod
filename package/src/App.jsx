3a8c57c7068f2580aeec90698044b66ebbaa8e77
adrianpm2002 <adrianpascual0708@gmail.com>
2025-05-14 19:01:50 -0400

    nav

--- package/src/App.jsx
+++ package/src/App.jsx
@@ -1,26 +1,27 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { useRoutes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // 🔹 Importamos lo necesario
 import Router from './routes/Router';
-
-
 import { baselightTheme } from "./theme/DefaultColors";
 
 function App() {
-  
-  const routing = useRoutes(Router);
-  const theme = baselightTheme;
+    const theme = baselightTheme;
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {routing}
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter> {/* ✅ Aseguramos que BrowserRouter envuelve las rutas */}
+                <Routes> {/* ✅ Cambiamos useRoutes a Routes */}
+                    {Router.map((route, index) => (
+                        <Route key={index} path={route.path} element={route.element}>
+                            {route.children?.map((child, i) => (
+                                <Route key={i} path={child.path} element={child.element} />
+                            ))}
+                        </Route>
+                    ))}
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
--- package/src/main.jsx
+++ package/src/main.jsx
@@ -1,12 +1,9 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
+
 
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
 import { ZonaProvider } from './context/ZonaContext'
 import { SensorProvider } from './context/SensorContext';
 import { UserProvider } from './context/userContext';
@@ -18,15 +15,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ZonaProvider>
       <UserProvider>
         <SensorProvider>
-          <BrowserRouter>
-            <ActivitiesProvider>
-              <DataSensorProvider>
 
-                <App />
+          <ActivitiesProvider>
+            <DataSensorProvider>
 
-              </DataSensorProvider>
-            </ActivitiesProvider>
-          </BrowserRouter>,
+              <App />
+
+            </DataSensorProvider>
+          </ActivitiesProvider>
+
         </SensorProvider>,
       </UserProvider>
     </ZonaProvider>,
